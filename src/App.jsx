3d4fd81ce5eb37c131a525e8a292bc263d6ddc7c
SergioCuadrado <sergiocuadrado14@gmail.com>
2023-02-23 21:11:03 +0100

    add: styles form

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,13 @@ import { ListPodcast } from './components/ListPodcast'
 function App () {
   return (
     <div>
-      <h1>Podcaster</h1>
+      <header>
+        <h1 className='title-app'>Podcaster</h1>
+        <form className='form'>
+          <strong>100</strong>
+          <input type='text' placeholder='Filter podcasts...' />
+        </form>
+      </header>
       <ListPodcast />
     </div>
   )
