cc1290fc49e8531740a4a2a569ead5219c0aa28d
SergioCuadrado <sergiocuadrado14@gmail.com>
2023-02-24 21:40:51 +0100

    delete: console.log

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,7 +5,7 @@ import { Loader } from '../Loader'
 import './styles.css'
 export const Header = () => {
   const { loading } = usePodcasts()
-  console.log(loading)
+
   return (
     <header className='header-podcaster'>
       <h1 className='title-app'><Link to='/'>Podcaster</Link></h1>
