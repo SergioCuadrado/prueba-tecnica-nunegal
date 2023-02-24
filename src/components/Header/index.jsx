5c12d1af3f524e665d6ca7af3ffc0084ca9e82d2
SergioCuadrado <sergiocuadrado14@gmail.com>
2023-02-24 19:49:37 +0100

    add: loader styles

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Loader } from '../Loader'
 import './styles.css'
 export const Header = () => {
   return (
-    <header>
+    <header className='header-podcaster'>
       <h1 className='title-app'><Link to='/'>Podcaster</Link></h1>
+      <Loader />
     </header>
   )
 }
